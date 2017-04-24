050674c6e2927ce6809f00b0c6596b04ed39590f
jk2K <andy1945jp@gmail.com>
2017-04-24 18:09:10 +0800

    Initial Commit

---
+++ leancloud-debug-sso/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  leancloud-debug-sso
+//
+//  Created by Meng Ye on 2017/4/24.
+//  Copyright © 2017年 jk2K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ leancloud-debug-ssoTests/leancloud_debug_ssoTests.swift
@@ -0,0 +1,36 @@
+//
+//  leancloud_debug_ssoTests.swift
+//  leancloud-debug-ssoTests
+//
+//  Created by Meng Ye on 2017/4/24.
+//  Copyright © 2017年 jk2K. All rights reserved.
+//
+
+import XCTest
+@testable import leancloud_debug_sso
+
+class leancloud_debug_ssoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
